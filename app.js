ceff287fc944793faf46a8dd04c4191dcda0982d
Nick Foden <nickfoden@gmail.com>
2017-07-08 13:50:31 -0400

    Add another view

--- app.js
+++ app.js
@@ -10,8 +10,39 @@ app.set('view engine', 'pug');
 
 //Home Route
 app.get('/', function(req, res){
-	res.render('index');
+	let articles =[
+		{
+			id:1,
+			title:"Article One",
+			author:"Nick",
+			body:"This is article One"
+		},
+		{
+			id:2,
+			title:"Article Two",
+			author:"Brad",
+			body:"This is article Two"
+		},
+		{
+			id:3,
+			title:"Article Three",
+			author:"Brian",
+			body:"This is article Three"
+		}
+	];
+	res.render('index', {
+		title:'Articles',
+		articles: articles
+	});
 });
+
+// Add Route
+app.get('/articles/add', function(req, res){
+	res.render('add_article', {
+		title:'Add Articles'
+	});
+});
+
 
 //start server
 app.listen(3000, function(){
