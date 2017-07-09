75ee85035cdc2d4ccdf122a474e60837e184a8ab
Nick Foden <nickfoden@gmail.com>
2017-07-09 16:19:24 -0400

    Esnureed only article owners can edit and delete articles

--- routes/articles.js
+++ routes/articles.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 //Bring in Article Model
 let Article = require('../models/article')
+//User Model
+let User = require('../models/user')
 
 // Add Route
-router.get('/add', function(req, res){
+router.get('/add', ensureAuthenticated, function(req, res){
 	res.render('add_article', {
 		title:'Add Article'
 	});
@@ -14,7 +16,7 @@ router.get('/add', function(req, res){
 // Add Submit Post Route
 router.post('/add', function(req, res){
 	req.checkBody('title', 'Title is Required').notEmpty();
-	req.checkBody('author', 'Author is Required').notEmpty();
+	//req.checkBody('author', 'Author is Required').notEmpty();
 	req.checkBody('body', 'Body is Required').notEmpty();
 
 	//Get Errors
@@ -28,7 +30,7 @@ router.post('/add', function(req, res){
 	} 	else {
 		let article = new Article();
 		article.title = req.body.title;
-		article.author = req.body.author;
+		article.author = req.user._id;
 		article.body = req.body.body;
 
 		article.save(function(err){
@@ -46,8 +48,12 @@ router.post('/add', function(req, res){
 });
 
 //Load Edit Form
-router.get('/edit/:id', function(req, res){
+router.get('/edit/:id', ensureAuthenticated, function(req, res){
 	Article.findById(req.params.id, function(err, article){
+		if(article.author != req.user_id){
+			req.flash('danger', 'Not Authorized');
+			res.redirect('/');
+		}
 		res.render('edit_article', {
 			title:'Edit Article',
 			article:article
@@ -77,23 +83,48 @@ router.post('/edit/:id', function(req, res){
 });
 
 router.delete('/:id', function(req, res){
+	if(!req.user._id){
+		res.status(500).send();
+	}
+
 	let query = {_id:req.params.id}
 
-	Article.remove(query, function(err){
-		if(err){
-			console.log(err);
+	Article.findById(req.params.id, function(err, article){
+		if(article.author != req.user._id){
+			res.status(500).send();
+		} else{
+			Article.remove(query, function(err){
+				if(err){
+					console.log(err);
+				}
+				res.send('Success');
+			});
 		}
-		res.send('Success');
 	});
 });
 
 //Get Single Article
 router.get('/:id', function(req, res){
 	Article.findById(req.params.id, function(err, article){
-		res.render('article', {
-			article:article
-		});
+		User.findById(article.author, function(err, user){
+			res.render('article', {
+				article:article,
+				author: user.name
+			});
+		})
 	});
 });
+
+// Access Control
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	} else {
+		req.flash('danger', 'Please Login');
+		res.redirect('/users/login');
+	}
+}
+
+
 
 module.exports = router;
