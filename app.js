b5c5f08f955f1c0a1d8b406b4fb2e2c20df5257c
Nick Foden <nickfoden@gmail.com>
2017-07-09 13:50:37 -0400

    Adding express messages

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const mongoose =  require('mongoose');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 mongoose.connect('mongodb://localhost/nodekb');
 
@@ -11,7 +14,6 @@ let db =  mongoose.connection;
 db.once('open', function(){
 	console.log('Connected to MongoDB');
 });
-
 
 //Check DB for Errors
 db.on('error', function(err){
@@ -34,6 +36,39 @@ app.use(bodyParser.json())
 
 // Set Public Folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Express Session Middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+
+//Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+//Express Validator Middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
 
 //Home Route
 app.get('/', function(req, res){
